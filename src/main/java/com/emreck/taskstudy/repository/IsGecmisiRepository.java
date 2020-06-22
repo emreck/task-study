ae688fb1ce1174d001f3f060ca1bce1148e427b8
emre.catamak@gmail.com <emre.catamak@gmail.com>
2020-06-22 17:40:38 +0300

    Revert "Hata"

--- src/main/java/com/emreck/taskstudy/repository/IsGecmisiRepository.java
+++
@@ -1,7 +0,0 @@
-package com.emreck.taskstudy.repository;
-
-import com.emreck.taskstudy.entity.IsGecmisi;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IsGecmisiRepository extends JpaRepository<IsGecmisi,Long> {
-}
